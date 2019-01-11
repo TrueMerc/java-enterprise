5ad13e2715a20f7572db27b5c5148af98c03620a
TrueMerc <true_merc@mail.ru>
2019-01-11 21:42:42 +0300

    Add commit before finish work this day.

--- webapp/src/main/java/ru/ryabtsev/enterprise/servlet/ErrorServlet.java
+++ webapp/src/main/java/ru/ryabtsev/enterprise/servlet/ErrorServlet.java
@@ -1,4 +1,23 @@
 package ru.ryabtsev.enterprise.servlet;
 
-public class ErrorServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Error servlet class.
+ */
+@WebServlet("/error")
+public class ErrorServlet extends HttpServlet {
+
+    private static final int HTTP_NOTFOUND_STATUS = 404;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("ERROR SERVLET");
+        resp.sendError(HTTP_NOTFOUND_STATUS);
+    }
 }
--- webapp/src/main/java/ru/ryabtsev/enterprise/servlet/MainServlet.java
+++ webapp/src/main/java/ru/ryabtsev/enterprise/servlet/MainServlet.java
@@ -6,14 +6,53 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Main page servlet class.
  */
 @WebServlet(urlPatterns = "/main")
 public class MainServlet extends HttpServlet {
+
+    private static final String PAGE_HEADER = "Main page";
+
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        PrintWriter out = resp.getWriter();
+//        out.println("<!DOCTYPE html>");
+//        out.println("<html>");
+//        out.println("<head>");
+//        out.println("<title>" + PAGE_HEADER + "</title>");
+//        out.println("</head>");
+//        out.println("<body>");
+//        out.println("<h1>" + PAGE_HEADER + "</h1>");
+//        out.println("</body>");
+//        out.println("</html>");
+//    }
+
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Main page");
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    protected void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Servlet response encoding: " + resp.getCharacterEncoding());
+        final PrintWriter out = resp.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>" + PAGE_HEADER + "</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>" + PAGE_HEADER + "</h1>");
+        out.println("</body>");
+        out.println("</html>");
+        System.out.println("Servlet response encoding: " + resp.getCharacterEncoding());
     }
 }
